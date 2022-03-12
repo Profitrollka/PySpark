00d5dba7338dbeba2916d4cddb79723c864954c4
Alena <alenak@MacBook-Air-Alena.local>
2022-03-12 16:26:56 +0300

    Added file with decorators

--- pysp_lib/decorators.py
+++ pysp_lib/decorators.py
@@ -13,10 +13,3 @@ def time_counter(func):
         print(f'Total time for function {func.__name__}: {str(end-start)[0]} hours {str(end-start)[2:4]} minutes {str(end-start)[5:7]} seconds')
         return result
     return wrapper
-
-
-@time_counter
-def summator(a):
-    return sum(a)
-
-summator(range(1000000000))
